c288ce04ceb36b2b31fcbecc2f488ecd6b56b8d4
Dwight <dwight.slaubaugh@gmail.com>
2022-08-24 15:54:16 -0500

    first commit

---
+++ Source/IdleAdventure/IdleAdventureGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "IdleAdventureGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class IDLEADVENTURE_API AIdleAdventureGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
